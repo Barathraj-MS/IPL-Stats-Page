a54ff76fe8fcf7df0a58a6aeffa061177d08b3c9
Barathraj MS <msbarathraj@gmail.com>
2022-03-04 23:50:09 +0530

    Added twitter feed backend

--- Server/index.js
+++ Server/index.js
@@ -1,18 +1,16 @@
 import  connection  from './db.js';
 import express from 'express';
 import cors from "cors";
+import fetch from 'node-fetch';
+import { authHeader } from './auth.js';
+// const authHeader =  {
+//     'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAAGuaOwEAAAAA9oVgYGjcm09nRYkzlrtHuHrqs04%3DyL56AWtCiFBFjePPHFzfAnggYZKqtPjz5RyqZOnJRNQhggNsSG',
+//     'Content-Type': 'application/json'
+// }
+
 const app = express();
-app.use(cors())
 
-
-// app.get('/players/batsman',(req,res)=>{
-//     connection.query("SELECT * FROM batsman", function(err,result){
-//         if(err) throw err;
-//         res.send(result);
-//     });
-// });
-
-
+app.use(cors({origin: true}))
 
 // NOTE: WHILE REQUESTING FROM SITE, REQUEST USING QUERY FOR EXAMPLE: /players/batsman/?filter=MostRuns , this to get the list of players sorted on runs.
 app.get('/players/batsman/', (req,res)=>{
@@ -29,6 +27,35 @@ app.get('/players/bowlers/', (req,res)=>{
         if(err) throw err;
         res.json(result);
     })
+})
+
+
+
+// Below API's are for feed page...
+var tweetmeta = [];
+var tweets = [];
+async function fetchTweets(userId){
+    const response = await fetch(`https://api.twitter.com/2/users/${userId}/tweets`, {
+        method: 'GET',
+        headers: authHeader
+    })
+    
+    await response.json().then(datas=>{
+        tweetmeta = datas.data;
+    })
+
+    // console.log(tweetmeta);
+    for(let i=0;i<tweetmeta.length;i++){
+        tweets.push(tweetmeta[i].text);
+    }
+    return tweets;
+    
+}
+
+app.get('/getfeed', async(req,res)=>{
+    const userId = "15639696";
+    const tweets = await fetchTweets(userId);
+    res.send(tweets);   
 })
 
 app.listen(3002, ()=>{
