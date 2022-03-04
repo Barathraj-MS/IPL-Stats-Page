161b8ef71bdd949d1588df2889f80d49b35dd878
Barathraj MS <msbarathraj@gmail.com>
2022-03-05 00:19:27 +0530

    Fetched Tweets from server and displayed on feed page(didnt style it yet)

--- src/Feed/Feed.js
+++ src/Feed/Feed.js
@@ -1,13 +1,29 @@
 import './Feed.css';
 import NavBar from '../NavBar/NavBar';
+import axios from 'axios';
+import React, { useEffect, useState } from "react";
+
 
 
 function Feed(){
+
+    const [tweets ,setTweets] = useState([]);
+
+    useEffect(()=>{
+        const response = axios.get('http://localhost:3002/getfeed')
+        .then((response)=>{
+            console.log(response.data);
+            setTweets(response.data);
+        })
+    }, [])
+
     return (
         <div>
             <NavBar />
             <div>
-                <p>Feed page</p>
+                <ol className="tweets">
+                    {tweets.map(tweet=><li>{tweet}</li>)}
+                </ol>
             </div>
         </div>
     )
